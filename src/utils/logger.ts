7946daa9584c15ad7ff2a5b06b91fe2518b66ad5
Alex van Steenhoven <alex.steenhoven@gmail.com>
2021-12-29 22:02:22 +0100

    changed name to new-express-project

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -5,7 +5,7 @@ export class Logger {
   // TODO: Create a logger with colored output
 
   public showHeader(): void {
-    console.log(cyan(figlet.textSync("create-express-app", { horizontalLayout: "default" })));
+    console.log(cyan(figlet.textSync("new-express-project", { horizontalLayout: "default" })));
     console.info(cyan(" Create a new express app but on steroids!"));
     console.log("\n \n");
   }
