cee57535c43fe41629e0ac7cc52ed1bc7c7e32d1
Alex van Steenhoven <alex.steenhoven@gmail.com>
2021-12-29 22:34:20 +0100

    Updated logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,12 +1,25 @@
-import { red, green, cyan } from "kleur";
+import { red, green, cyan, gray } from "kleur";
 import figlet from "figlet";
+
+import moment from "moment";
 
 export class Logger {
   // TODO: Create a logger with colored output
+  private showDate(): string {
+    return gray(`[${moment(new Date()).format("DD/MM/YYYY, hh:mm:ss")}]`);
+  }
+
+  private showLoglevel(level: string): string {
+    return cyan(`[${level}]`);
+  }
 
   public showHeader(): void {
     console.log(cyan(figlet.textSync("new-express-project", { horizontalLayout: "default" })));
     console.info(cyan(" Create a new express app but on steroids!"));
     console.log("\n \n");
+  }
+
+  public info(message: string): void {
+    console.log(`${this.showDate()} ${this.showLoglevel("INFO")} - ${message}`);
   }
 }
